9875ba88b421a085ab7ccaaa3533bedef5d3125e
Richard Boyne <boynerichard@yahoo.co.uk>
2019-08-08 15:06:53 +0000

    cleaned SLIC and MSLIC
    numerious small changes including hiding and renaming vairablese, will need to have the tests edited

--- kmeans/MSLIC.py
+++ kmeans/MSLIC.py
@@ -2,149 +2,154 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 from SLIC import SLIC
-from tools import set_seed, get_img, progress_bar
+from tools import progress_bar
 
 
 class MSLIC_wrapper():
+    """
+    Wrapper around SLIC to run multiple instances at once, using a the
+    combined of each instance to assign clusters in every iteration
 
-    def __init__(self, imgs, bin_grid, combo_metric='max',
-                 dist_metric='default', **kwargs):
-        """
-        Wrapper around SLIC to run multiple instances at once, using a the
-        combined of each instance to assign clusters in every iteration
-        
-        Parameters
-        ----------
-        
-        imgs : list of 3d numpy arrays
-            The imgs on which the SLIC algorithm is to be run in parallel. 
-            Each must have the same shape and have a 
-            
-        bin_grid : tuple length 2 (same as SLIC)
-            The number of initial partitions in x and y respectivly,
-            therefore the number of segments is the product of these. 
-            Both must be a factor of the x, y dimensions for the whole
-            image. This intial segmentation restrains the kmeans centers
-            in space, forcing locality of segments and speeding up the
-            algorithm.
-            
-        combo_metric : string, optional
-            The metric by which the distances between vectors and centroids
-            in each image are to be combined. Allowed options are:
-            - 'max' maximum of the distances in each image
-            - 'min' minimum of the distances in each image
-            - 'mean' average of the distances in each image
-            - 'sum' total of the distances in each image
-            - 'custom' custom function must be passed as 'metric_func' kwarg
-                which takes a 3d tensor of distances with shape
-                (image, vector, cluster) and returns a 2d tensor of distances
-                with shape (vector, cluster)
+    Parameters
+    ----------
 
-        dist_metric : string, optional (same as SLIC)
-            Choose the of calculating distance between two vectors:
-            - 'normal' finds the normal distance without scaling either
-                position or color space
-            - 'default' finds the normal distance with scaling the position
-                space by the bin widths. Can also pass the kwarg 'factor'
-                with an additional scaling factor for the position space.
-            - 'custom' pass a fucntion to be used in the kwarg 'dist_func'
-            
-        kwargs : optional
-            Other than those described above for combo_metric and dist_metric
-            these are passed into the SLIC objects, refer to it's documentaiton.
-        """
+    imgs : list of 3d numpy arrays
+        The imgs on which the SLIC algorithm is to be run in parallel. 
+        Each must have the same shape and have a 
+
+    bin_grid : tuple length 2 (same as SLIC)
+        The number of initial partitions in x and y respectivly,
+        therefore the number of segments is the product of these. 
+        Both must be a factor of the x, y dimensions for the whole
+        image. This intial segmentation restrains the kmeans centers
+        in space, forcing locality of segments and speeding up the
+        algorithm.
+
+    combo_metric : string or callable, optional
+        The metric by which the distances between vectors and centroids
+        in each image are to be combined. Allowed string options are:
+        - 'max' maximum of the distances in each image
+        - 'min' minimum of the distances in each image
+        - 'mean' average of the distances in each image
+        - 'sum' total of the distances in each image
+        
+        Alternatively a callable function can be used, this function must
+        have form f(t, *args) where t is a rank 3 tensor of distances with
+        shape (img, vectors, clusters) and return a rank 2 tensor of distances
+        with shape (vectors, clusters).
+      
+    combo_metric_args : tuple, optional
+        arguments to passed into combo_metric function if given
+      
+    dist_metric : function, optional
+        Passed into SLIC objects.
+        The method of calculating distance between vectors and cluster
+        centers. This must have form f(vecs, clusts, *args) where vecs and
+        clusts are rank 2 tensors with samples on the first axis. This
+        should return a rank 2 tensor of distances with shape (clusters, vectors)
+        such that taking the argmin along dim=1 gives an array of closest
+        cluster centroids for each vector
+
+    dist_metric_args : tuple, optional
+        Passed into SLIC objects.
+        arguments to passed into dist_metric function if given
+        
+    """
+
+    _default_combo_metrics = {'max': lambda t,*args : t.max(dim=0)[0],
+                              'min': lambda t,*args : t.min(dim=0)[0],
+                              'mean': lambda t,*args : t.mean(dim=0),
+                              'sum': lambda t,*args : t.sum(dim=0)}
+    
+    def __init__(self, imgs, bin_grid, combo_metric='max', combo_metric_args=[],
+                 dist_metric=None, dist_metric_args=[]):
         
         # validate all given images
         for img in imgs:
             assert img.shape == imgs[0].shape, "images must be the same shape"
         
         # create the SLIC objects
-        self.SLIC_objs = [SLIC(img, bin_grid, dist_metric, **kwargs)
+        self.SLIC_objs = [SLIC(img, bin_grid, dist_metric, dist_metric_args)
                           for img in imgs]
+                            
+        self.metric_args = combo_metric_args
         
-        # set the combine metric
-        if combo_metric == 'max':
-            self.metric = lambda t:t.max(dim=0)[0]
-
-        elif combo_metric == 'min':
-            self.metric = lambda t:t.min(dim=0)[0]
-
-        elif combo_metric == 'mean':
-            self.metric = lambda t:t.mean(dim=0)
-
-        elif combo_metric == 'sum':
-            self.metric = lambda t:t.sum(dim=0)
-            
-        elif combo_metric == 'custom':
-            assert 'metric_func' in kwargs.keys(), \
-                "Must pass 'metric_func' as key word argument"
-            self.metric = metric_func
-        
+        if not callable(combo_metric):
+            assert combo_metric in self._default_combo_metrics.keys(), \
+                "metric " + combo_metric + " not recognised"
+            self.metric = self._default_combo_metrics[combo_metric]
         else:
-            assert False, "metric " + combo_metric + " not recognised"
+            self.metric = combo_metric
                 
     
-    def combined_distance(self):
-        "Find the combined distance for every pixel using the specified metric"
+    def _combined_distance(self):
+        """
+        Find the combined distance for every pixel in every bin using the
+        given combo_metric
+        """
         
         # reset the distances
-        self.combo_dist = []
+        combo_dist = []
         
-        # for all distance values in every bin
-        all_distances = [obj.vc_dists for obj in self.SLIC_objs]
+        # collect the distances for every SLIC in every bin
+        all_distances = [obj.distances for obj in self.SLIC_objs]
+        
+        # zip together the bins and loop over them
         for dists in zip(*all_distances):
             
-            # calculate the combined distance using the distance metric
+            # calculate the combined distance for this bin using combo_metric
             dist_tensor = torch.stack(dists)
-            self.combo_dist.append(self.metric(dist_tensor))
+            combo_dist.append(self.metric(dist_tensor))
+        
+        return combo_dist
     
             
     def iterate(self, n_iter):
         "loop for n_iter iterations with progress bar"
         
-        # create the progress bar and store on each of the objects
-        self.progress_bar = progress_bar(n_iter)
-#         for obj in self.SLIC_objs:
-#             obj.progress_bar = self.progress_bar
+        # create the progress bar
+        self._progress_bar = progress_bar(n_iter)
         
         for i in range(n_iter):
             
             # iterate up to finding distances for each kmeans object
             for obj in self.SLIC_objs:
-                obj.update_distances()
+                obj._update_distances()
                 
             # find the combined distance
-            self.combined_distance()
+            combo_dist = self._combined_distance()
             
             # finish the iteration for each SLIC
             for n, obj in enumerate(self.SLIC_objs):
                 
                 # set the combined distance
-                obj.vc_dists = self.combo_dist
+                obj._distances = combo_dist
                 
-                # only calculate the new clusters once
+                # only calculate the new clusters once as this is same for all
                 if n == 0:
-                    obj.update_clusters()
+                    obj._update_clusters()
                 else:
-                    obj.cluster_tensor = self.SLIC_objs[0].cluster_tensor
-                    obj.cluster_list = self.SLIC_objs[0].cluster_list
+                    obj.vec_clusts = self.SLIC_objs[0].vec_clusts
+                    obj._cluster_contense = self.SLIC_objs[0]._cluster_contense
                     
                 # update the centroid means
-                obj.update_centroids()
+                obj._update_centroids()
             
             # print the progress bar
-            self.progress_bar(i)
+            self._progress_bar(i)
     
     
-    def plot(self, obj_indexs, option='default', axs=[None], path=None):
+    def plot(self, objs=[], option='default', axs=[None], path=''):
         """
-        Calls obj.plot() on each of the SLIC objects given with option
+        Calls obj.plot() on each of the SLIC objects specified with option
+        given
         
         Parameters
         ----------
         
-        obj_indexs : list of ints
-            which of the given objects to be plotted
+        obj_indexs : list of ints, optional
+            which of the SLIC objects to call plot on. If left empty this
+            will plot all SLIC objects.
             
         option : string, optional
             the plot option to be passed to obj.plot, refer to the docstrings
@@ -152,45 +157,57 @@ class MSLIC_wrapper():
             
         axs : tuple of matplotlib axis, optional
             the axis to be plotted on, if not given a subplot for each of
-            the SLIC objs is used
+            the SLIC objs is used.
+            
+        path : string, optional
+            the path to save the figure on if wanted
         """
         
-        # if the time plot is wanted just use own time plot
+        # if the time plot is wanted use time plot on this wrapper
         if option == 'time':
             self.progress_bar.plot_time()
             return
         
+        # if default objs set then replace will list of all indexs
+        if len(objs) == 0:
+            objs = range(len(self.SLIC_objs))
+        
         # set the axis if not given
         if not any(axs):
-            N = len(obj_indexs)
+            N = len(objs)
             fig, axs = plt.subplots(N, 1, figsize=[N*22, 22])
             axs = np.array([axs]).ravel() # force it be an array even if N = 1
         
         # call the plot routenes
-        for i, ax in zip(obj_indexs, axs):
-            self.SLIC_objs[i].plot(option, ax=ax, path=path)
+        for i, ax in zip(objs, axs):
+            self.SLIC_objs[i].plot(option, ax=ax)
+                      
+        # save the figure if wanted
+        if path:
+            plt.savefig(path)
             
             
 if __name__ == '__main__':
-    set_seed(10)
-
-    grid = [40, 40]
-
-    img_white = get_img("images/TX1_white_cropped.tif")
-    obj_white = kmeans_local(img_white, grid)
-
-    img_polar = get_img("images/TX1_polarised_cropped.tif")
-    obj_polar = kmeans_local(img_polar, grid)
-
-    # plot the initial binning 
-    obj_white.plot("setup")
-    plt.gca().set(title='Initial Grid')
-
-    multi_obj = MSLIC_wrapper([obj_white, obj_polar])
-
-    # iterate
-    multi_obj.iterate(2)
+    # run an example of SLIC on two images, then MSLIC on both
+    from tools import get_img
     
-    # plot
-    multi_obj.plot([0,1])
+    # setup
+    grid = [40, 40]
+    img_white = get_img("images/TX1_white_cropped.tif")
+    img_polar = get_img("images/TX1_polarised_cropped.tif")
+    
+    # iterate SLIC with just the white image
+    obj_white = SLIC(img_white, grid)
+    obj_white.iterate(10)
+    obj_white.plot()
+    
+    # iterate SLIC with just the polar image
+    obj_polar = SLIC(img_polar, grid)
+    obj_polar.iterate(10)
+    obj_polar.plot()
+    
+    # iterate MSLIC with both white and polar images
+    obj_both = MSLIC_wrapper([img_white, img_polar], grid)
+    obj_both.iterate(10)
+    obj_both.plot()
     
