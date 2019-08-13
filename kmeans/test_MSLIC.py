e8f11ed6952c97afdf1102d2c484f0113c0e2d35
Richard Boyne <boynerichard@yahoo.co.uk>
2019-08-13 13:45:50 +0000

    Small changes to MSLIC and added extra tests
    A default arg added for scaling the color-position space. Tests were updated and an acceptance test was introduced

--- kmeans/test_MSLIC.py
+++ kmeans/test_MSLIC.py
@@ -28,16 +28,17 @@ class Test_MSLIC_wrapper(unittest.TestCase):
         
         # find the combined distance
         for o in obj.SLIC_objs:
-            o.update_distances()
-        obj.combined_distance()
+            o._update_distances()
+        combo_dist = obj._combined_distance()
         
         # test the combined distance makes sense
         assert all([a.shape == b.shape for a,b in
-                    zip(obj.combo_dist, obj.SLIC_objs[0].vc_dists)]),\
+                    zip(combo_dist, obj.SLIC_objs[0].distances)]),\
             "the combined distance must have the same shape as the " +\
             "distances for each SLIC object"
+        
         assert all([a.shape == b.shape for a,b in
-                    zip(obj.combo_dist, obj.SLIC_objs[1].vc_dists)]),\
+                    zip(combo_dist, obj.SLIC_objs[1].distances)]),\
             "the combined distance must have the same shape as the " +\
             "distances for each SLIC object"
         
@@ -57,8 +58,8 @@ class Test_MSLIC_wrapper(unittest.TestCase):
         obj.iterate(1)
         
         # check both SLIC instances agree on the clustering
-        assert all(obj.SLIC_objs[0].cluster_tensor == \
-                   obj.SLIC_objs[1].cluster_tensor),\
+        assert all(obj.SLIC_objs[0].vec_clusts == \
+                   obj.SLIC_objs[1].vec_clusts),\
             "both SLIC instances must agree on the clustering"
         
         # check the SLIC instances are still functional
@@ -73,29 +74,32 @@ class Test_MSLIC_wrapper(unittest.TestCase):
         """
         
         # test distance list makes sense
-        assert len(obj.vc_dists) == 16, "should be 16 bin distance tensors"
-        assert obj.vc_dists[0].dtype == torch.float32, \
+        assert len(obj.distances) == 16, "should be 16 bin distance tensors"
+        
+        assert obj.distances[0].dtype == torch.float32, \
             "vc_dists should be a tensor with float values"
+        
         assert all([a.shape[0] == b.shape[0] for a, b in 
-                    zip(obj.vc_dists, obj.vec_bins_list)]), \
+                    zip(obj.distances, obj._bins_list)]), \
             "The number of distance measures in each bin must match the " +\
             "number of vectors in that bin"
+        
         assert all([a.shape[1] == b.shape[0] for a, b in 
-                    zip(obj.vc_dists, obj.adj_bins)]), \
+                    zip(obj.distances, obj._adj_bins)]), \
             "The number of distance measures per vector must match the " +\
             "number of adjasent centers for that bin"
 
         # test clusters make sense
-        assert sum([o.numel() for o in obj.cluster_list]) == obj.Np, \
+        assert sum([o.numel() for o in obj._cluster_contense]) == obj._Np, \
             "clustered vectors needs to be same as number of pixels"
-        assert obj.cluster_tensor.unique().numel() == 16, \
+        
+        assert obj.vec_clusts.unique().numel() == 16, \
             "should be exactly 16 clusters" 
-    
         
         # test the new cetroids are still inside the image domain
         # note the * here acts as an and opterator
-        tmp = ((obj.centroids[:, 0] <= obj.dim_x) * (obj.centroids[:, 0] >= 0) *
-               (obj.centroids[:, 1] <= obj.dim_y) * (obj.centroids[:, 1] >= 0) *
+        tmp = ((obj.centroids[:, 0] <= obj._dim_x) * (obj.centroids[:, 0] >= 0) *
+               (obj.centroids[:, 1] <= obj._dim_y) * (obj.centroids[:, 1] >= 0) *
                (obj.centroids[:, 2] <= 1) * (obj.centroids[:, 2] >= 0) *
                (obj.centroids[:, 3] <= 1) * (obj.centroids[:, 3] >= 0) *
                (obj.centroids[:, 4] <= 1) * (obj.centroids[:, 4] >= 0))
@@ -114,7 +118,7 @@ class Test_MSLIC_wrapper(unittest.TestCase):
         obj = MSLIC_wrapper((img_w, img_p), [4,4])
         
         # iterate
-        obj.iterate(1)
+        obj.iterate(2)
         
         for opt in ['default', 'setup', 'edges', 'img', 'centers', 
                     'bins', 'time', 'bin_edges', 'segments', 'setup']:
@@ -126,5 +130,46 @@ class Test_MSLIC_wrapper(unittest.TestCase):
         obj.plot([1], 'default')
         
         
+    def test_result(self):
+        """
+        acceptance test that the outcome is as expected for a simple image by
+        checking a handful of manually identified points are seperated by the
+        algorithm
+        """
+        # setup and iterate
+        set_seed(10)
+        img_w = get_img("images/TX1_white_cropped.tif")
+        img_p = get_img("images/TX1_polarised_cropped.tif")
+        obj = MSLIC_wrapper((img_w, img_p), [4,4])
+        obj.iterate(5)
+        
+        # extract mask
+        mask = obj.get_segmentation()
+        
+        # define known points
+        known_points = [[10, 10],
+                        [300, 10],
+                        [10, 500],
+                        [10, 400],
+                        [850, 200],
+                        [850, 100]]
+        
+        # for each pair check they are in different segments
+        for x1, y1 in known_points:
+            for x2, y2 in known_points:
+                if x1 != x2 and y1 != y2:
+                    assert mask[y1, x1] != mask[y2, x2], \
+                        "Mask does not agree with expected"
+
+    
 if __name__ == '__main__':
-    unittest.main()
+    
+    # run all the tests if this is script is run independently
+    test_obj = Test_MSLIC_wrapper()
+    test_obj.test_distance_combination()
+    test_obj.test_assignment_to_SLIC()
+    test_obj.test_plot()
+    test_obj.test_result()
+    
+#     # unittest.main does not work in google colab, but should work elsewhere
+#     unittest.main()
